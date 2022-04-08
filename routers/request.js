654977c7f1c97f946b6eea7c425725bdcfae3487
Jorie MacDonald <joriem@me.com>
2022-04-08 14:06:37 -0400

    updated controllers and routers for requests

---
+++ routers/request.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { request } from '../controllers/request';
+
+const router = Router();
+
+// get all requests and create a new request
+router.route('/')
+  .get(request.readAll)
+  .post(request.create);
+// not entirely sure how to fetch such that only shows requests which are relevant?
+
+// fetch calendar event by id, update event by id, remove event by id
+router.route('/:id')
+  .get(request.read)
+  .put(request.update)
+  .delete(request.deleteEvent);
+
+export default router;
--- src/controllers/request.js
+++ src/controllers/request.js
@@ -2,9 +2,47 @@ import { useResolvedPath } from 'react-router-dom';
 import Request from '../models/request';
 
 // create request
-// get request
-// update request
+const create = async (req, res) => {
+    try {
+      const newRequest = await Request.create(req.body);
+      res.status(200).json(newRequest);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
+// read request with particular idea
+const read = async (req, res) => {
+    console.log('read');
+    try {
+      const foundRequest = await Requst
+        .findById(req.params.id);
+      //   .populate('author')
+      //   .populate('approvals');
+      console.log(foundRequest);
+      res.status(200).json(foundRequest);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
+// update request; updates selected request
+const update = async (req, res) => {
+    try {
+      const updatedRequest = await Request.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      res.status(200).json(updatedRequest);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
 // delete request
+const deleteRequest = async (req, res) => {
+    try {
+      await Request.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: 'delete success' });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
+  
 //get all requests 
 export const getAllRequests = async () => {
     try {
@@ -18,3 +56,16 @@ export const getAllRequests = async () => {
         throw error;
     }
 }
+
+const request = {
+  create,
+  read,
+  update, 
+  deleteRequest,
+  getAllRequests,
+  
+
+
+};
+
+export default request;
