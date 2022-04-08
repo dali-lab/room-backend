2d3b6babe179faa7cea12404bd713324996cc6b5
kksamps2 <kksamps2@gmail.com>
2022-04-08 15:45:00 -0400

    fixed auth

--- src/routers/calendarEventRouter.js
+++ src/routers/calendarEventRouter.js
@@ -1,17 +1,18 @@
 import { Router } from 'express';
 import { calendarEventController } from '../controllers';
+import { requireAuth } from '../authentication';
 
 const router = Router();
 
 // get all calendar events and create new calendar event
 router.route('/')
-  .get(calendarEventController.readAll)
-  .post(calendarEventController.create);
+  .get(requireAuth, calendarEventController.readAll)
+  .post(requireAuth, calendarEventController.create);
 
 // fetch calendar event by id, update event by id, remove event by id
 router.route('/:id')
-  .get(calendarEventController.read)
-  .put(calendarEventController.update)
-  .delete(calendarEventController.deleteEvent);
+  .get(requireAuth, calendarEventController.read)
+  .put(requireAuth, calendarEventController.update)
+  .delete(requireAuth, calendarEventController.deleteEvent);
 
 export default router;
