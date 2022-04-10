dd2569225d9919e6902d2a076f04fe31b4b5ba07
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2022-04-09 20:14:42 -0400

    got rid of auth for calendar events

--- src/routers/calendarEventRouter.js
+++ src/routers/calendarEventRouter.js
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import { calendarEventController } from '../controllers';
-import { requireAuth } from '../authentication';
+// import { requireAuth } from '../authentication';
 
 const router = Router();
 
 // get all calendar events and create new calendar event
 router.route('/')
-  .get(requireAuth, calendarEventController.readAll)
-  .post(requireAuth, calendarEventController.create);
+  .get(calendarEventController.readAll)
+  .post(calendarEventController.create);
 
 // fetch calendar event by id, update event by id, remove event by id
 router.route('/:id')
-  .get(requireAuth, calendarEventController.read)
-  .put(requireAuth, calendarEventController.update)
-  .delete(requireAuth, calendarEventController.deleteEvent);
+  .get(calendarEventController.read)
+  .put(calendarEventController.update)
+  .delete(calendarEventController.deleteEvent);
 
 export default router;
