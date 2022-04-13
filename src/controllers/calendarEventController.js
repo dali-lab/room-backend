7b4480390d0fb6f553b8cb2c0cdecafdd85a8c5f
kksamps2 <kksamps2@gmail.com>
2022-04-13 14:35:56 -0400

    fixed to find userIds in readAll

--- src/controllers/calendarEventController.js
+++ src/controllers/calendarEventController.js
@@ -59,7 +59,7 @@ const deleteEvent = async (req, res) => {
 const readAll = async (req, res) => {
   try {
     const allEvents = await CalendarEvent
-      .find(req.query)
+      .find({ author: { $in: req.query.userIds } })
       .populate('author')
       .populate('approvals');
     res.status(200).json(allEvents);
