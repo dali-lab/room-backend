3ea27bd8e9808bb3f7c5d8e1d352047c6dfca665
chelseajoe <chelseaj2021@gmail.com>
2022-05-09 16:40:17 -0400

    reset

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -150,7 +150,8 @@ export const sendTestEmail = async (email) => {
  */
 export const resetPassword = async (req, res) => {
   console.log('userController');
-  const user = (await User.findOne({ email: req.body.email }));
+  const user = await User
+    .findOne({ email: req.body.email });
   console.log(user);
 
   // if (!user) {
