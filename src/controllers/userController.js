bad93c2aae6793d1c8a6ad7f9142348efbe5b220
Jessica Cheng <jessica.cheng.23@dartmouth.edu>
2022-05-09 22:47:02 -0400

    updates

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -129,6 +129,7 @@ export const changePassword = async (id, newPassword) => {
 };
 
 export const sendTestEmail = async (email) => {
+  console.log('sending test email');
   const msg = {
     from: 'noreply.insectinvasionmodel@gmail.com',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
