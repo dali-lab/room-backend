8332a637c7f07ae9ffc24eea9170d491e127c066
chelseajoe <chelseaj2021@gmail.com>
2022-05-13 16:17:45 -0400

    update

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -16,9 +16,6 @@ router.route('/signup')
 router.route('/reset-password')
   .put(userController.resetPassword);
 
-router.route('/reset-password/:password')
-  .put(userController.resetPassword);
-
 router.route('/:id')
   .get(requireAuth, userController.read)
   .put(requireAuth, userController.update)
--- src/server.js
+++ src/server.js
@@ -55,8 +55,6 @@ app.use((req, res) => {
 });
 
 sgMail.setApiKey(process.env.SG_API_KEY);
-console.log(process.env.SG_API_KEY);
-console.log(process.env.port);
 
 // START THE SERVER
 // =============================================================================
